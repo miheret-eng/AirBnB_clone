bffde41727e9aa7f09af9a4657122d5a7fa7cdc6
aloicerains <aloiceokoth98@gmail.com>
2021-11-12 21:41:48 +0300

    added engine

---
+++ models/engine/file_storage.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""FileStorage module"""
+
+from models.base_model import BaseModel
+import json
+
+class FileStorage:
+    """Serializes to JSON file and deserializes JSON file to instances
+
+    Attributes:
+        __file_path(str): path to JSON file
+        __objects(dict): dictionary to store all objects
+
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary 
+        Returns:
+            __objects(dict): dictionary of objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in dictionary the obj with key <obj class name>.id"""
+        if obj is not None:
+            name = type(obj).__name__ + '.' + obj.id
+            self.__objects[name] = obj
+
+    def save(self):
+        """Serializes objects to the JSON file"""
+        json_ob = {}
+        for key in self.__objects:
+            json_ob[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, 'w', encoding = 'utf8') as json_file:
+            json.dump(json_ob, json_file)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects"""
+        try:
+            with open(self.__file_path, 'r') as json_file:
+                obj_dict = json.load(json_file)
+            for key, value in obj_dict.items():
+                self.__objects[key] = value
+        except:
+            pass
+                
+
