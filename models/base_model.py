03c7f19d85871c0b802c5ce6f7515a99953fc267
aloicerains <aloiceokoth98@gmail.com>
2021-11-14 21:37:48 +0300

    updatedn

--- models/base_model.py
+++ models/base_model.py
@@ -1,65 +1,61 @@
 #!/usr/bin/python3
-"""BaseModel Module"""
+"""
+Contains class BaseModel
+"""
 
+from datetime import datetime
 import models
 import uuid
-import datetime
-import pytz
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
-    """class defines common attributes and methods for other classes"""
+    """The BaseModel class from which future classes will be derived"""
 
     def __init__(self, *args, **kwargs):
-        """Initializes the BaseModel class instances"""
-        if kwargs is not None and len(kwargs) > 0:
-            if "id" in kwargs.items():
-                pass
-            else:
-                self.id = str(uuid.uuid4())
-            if "created_at" in kwargs.items():
-                pass
-            else:
-                self.created_at = datetime.datetime.now()
-            if "upated_at" in kwargs.items():
-                pass
-            else:
-                self.updated_at = datetime.datetime.now()
+        """Initialization of the base model"""
+        if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
-                    if key == "created_at":
-                        self.created_at = datetime.datetime.\
-                                strptime(kwargs[key], "%Y-%m-%dT%H:%M:%S.%f")
-                        continue
-                    elif key == "updated_at":
-                        self.updated_at = datetime.datetime.\
-                                strptime(kwargs[key], "%Y-%m-%dT%H:%M:%S.%f")
-                        continue
-                    else:
-                        setattr(self, key, value)
+                    setattr(self, key, value)
+            if kwargs.get("created_at", None) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+            else:
+                self.updated_at = datetime.utcnow()
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
+            self.created_at = datetime.utcnow()
             self.updated_at = self.created_at
 
     def __str__(self):
-        """string representation of class"""
+        """String representation of the BaseModel class"""
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
                                          self.__dict__)
 
     def save(self):
-        """Updates the attribute updated_at with current datetime"""
-        self.updated_at = datetime.datetime.now()
+        """updates the attribute 'updated_at' with the current datetime"""
+        self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self):
-        """Returns the diictionry of key/value of the instance"""
-        attributes = {"id": self.id, "created_at": self.created_at.isoformat(),
-                      "updated_at": self.updated_at.isoformat()}
+    def to_dict(self, save_fs=None):
+        """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
-        for attr in attributes:
-            if attr in new_dict:
-                new_dict[attr] = attributes[attr]
+        if "created_at" in new_dict:
+            new_dict["created_at"] = new_dict["created_at"].strftime(time)
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
+        if "_sa_instance_state" in new_dict:
+            del new_dict["_sa_instance_state"]
+        if save_fs is None:
+            if "password" in new_dict:
+                del new_dict["password"]
         return new_dict
