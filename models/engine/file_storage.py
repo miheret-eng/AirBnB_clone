3d46a40e6c2c053bf48e6cf5d47ae07cedc60b28
aloicerains <aloiceokoth98@gmail.com>
2021-11-15 09:14:07 +0300

    deleted

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,43 +18,48 @@ classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
 
 
 class FileStorage:
-    """serializes instances to a JSON file & deserializes back to instances"""
+    """The class serializes to JSON file and back to objects
+
+    Attributes:
+        __file_path(str): path to the json file
+        __objects(dict): dictionary of all objects created
+
+    """
 
     __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
-        """returns the dictionary __objects"""
+        """Returns  __objects"""
         if cls is not None:
             new_dict = {}
-            for key, value in self.__objects.items():
-                if cls == value.__class__ or cls == value.__class__.__name__:
-                    new_dict[key] = value
+            for k, v in self.__objects.items():
+                if cls == v.__class__ or cls == v.__class__.__name__:
+                    new_dict[k] = v
             return new_dict
         return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
+        """Sets in __objects obj using key <obj class name>.id"""
+
         if obj is not None:
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
-        json_objects = {}
+        """Serializes __objects to the JSON file"""
+        json_objs = {}
         for key in self.__objects:
-            if key == "password":
-                json_objects[key].decode()
-            json_objects[key] = self.__objects[key].to_dict(save_fs=1)
+            json_objs[key] = self.__objects[key].to_dict()
         with open(self.__file_path, 'w') as f:
-            json.dump(json_objects, f)
+            json.dump(json_objs, f)
 
     def reload(self):
-        """deserializes the JSON file to __objects"""
+        """Deserializes JSON file to __objects"""
         try:
             with open(self.__file_path, 'r') as f:
-                jo = json.load(f)
-            for key in jo:
-                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
+                js = json.load(f)
+            for key in js:
+                self.__objects[key] = classes[js[key]["__class__"]](**js[key])
         except:
             pass
