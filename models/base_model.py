c0a59d038073fb228455acbd212efb375ab712b6
aloicerains <aloiceokoth98@gmail.com>
2021-11-15 09:13:45 +0300

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-"""
-Contains class BaseModel
-"""
+"""BaseModel module"""
 
 from datetime import datetime
 import models
 import uuid
 
-time = "%Y-%m-%dT%H:%M:%S.%f"
-
 
 class BaseModel:
-    """The BaseModel class from which future classes will be derived"""
+    """The BaseModel for subsequen future classes"""
 
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
+
+        frmt = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
             if kwargs.get("created_at", None) and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], time)
+                self.created_at = datetime.strptime(kwargs["created_at"], frmt)
             else:
                 self.created_at = datetime.utcnow()
             if kwargs.get("updated_at", None) and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+                self.updated_at = datetime.strptime(kwargs["updated_at"], frmt)
             else:
                 self.updated_at = datetime.utcnow()
             if kwargs.get("id", None) is None:
@@ -35,27 +33,26 @@ class BaseModel:
             self.updated_at = self.created_at
 
     def __str__(self):
-        """String representation of the BaseModel class"""
+        """String representation BaseModel class"""
+
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
                                          self.__dict__)
 
     def save(self):
-        """updates the attribute 'updated_at' with the current datetime"""
+        """updates  the current time"""
+
         self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self, save_fs=None):
-        """returns a dictionary containing all keys/values of the instance"""
+    def to_dict(self):
+        """Creates a dictionary with kvp for each instance"""
+
+        frmt = "%Y-%m-%dT%H:%M:%S.%f"
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime(time)
+            new_dict["created_at"] = new_dict["created_at"].strftime(frmt)
         if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+            new_dict["updated_at"] = new_dict["updated_at"].strftime(frmt)
         new_dict["__class__"] = self.__class__.__name__
-        if "_sa_instance_state" in new_dict:
-            del new_dict["_sa_instance_state"]
-        if save_fs is None:
-            if "password" in new_dict:
-                del new_dict["password"]
         return new_dict
